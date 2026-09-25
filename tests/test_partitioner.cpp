a4db752d25895d182e127d736c15933e71694642
Willem Sonke <willem@wimiso.nl>
2026-09-25 11:54:16 +0200

    Add copyright headers

--- tests/test_partitioner.cpp
+++ tests/test_partitioner.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2025-2026 TU Eindhoven
+//
+// This file is part of merge-tree-interleavings.
+// 
+// merge-tree-interleavings is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// merge-tree-interleavings is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// merge-tree-interleavings. If not, see <https://www.gnu.org/licenses/>.
+
 #include "catch.hpp"
 
 #include "partitioner.h"
